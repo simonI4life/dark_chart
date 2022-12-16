90bb9f7ca09fd8e6aa063d6a6561287ccc0e9097
Simon <simon@i4life.es>
2022-12-16 13:40:05 +0100

    new

--- dinamic/main.js
+++ dinamic/main.js
@@ -5,6 +5,16 @@ function createChart(id, data, options) {
   return new Chart(ctx, { type: "line", data: data, options: options });
 }
 
+function createChart2(id, data, options) {
+  var ctx = document.getElementById(id).getContext("2d");
+  return new Chart(ctx, { type: "line", data: data, options: options });
+}
+
+let currentDate = new Date();
+let previousMonth = new Date();
+
+let mode = 0;
+
 const options = {
   plugins: {
     legend: {
@@ -23,6 +33,7 @@ const options = {
       time: {
         unit: "month",
       },
+
       grid: {
         color: "#e1e1e123",
       },
@@ -52,51 +63,247 @@ function createData(jsonData, colors) {
   return salesData;
 }
 
-const generateDateArray = (number) => {
-  date = [];
-  for (i = 0; i <= number; i++) {
-    date.push(new Date(`2022-${i + 1}-01 00:00:00`));
+const sourceData1 = [
+  {
+    date: new Date("9-1-2021"),
+    d: [0, 0],
+  },
+  {
+    date: new Date("9-15-2021"),
+    d: [885, 892],
+  },
+  {
+    date: new Date("10-20-2021"),
+    d: [991, 989],
+  },
+  {
+    date: new Date("12-01-2021"),
+    d: [1113, 1086],
+  },
+  {
+    date: new Date("01-02-2022"),
+    d: [1221, 1184],
+  },
+  {
+    date: new Date("02-12-2022"),
+    d: [1321, 1352],
+  },
+  {
+    date: new Date("03-13-2022"),
+    d: [1359, 1425],
+  },
+  {
+    date: new Date("03-19-2022"),
+    d: [2020, 2071],
+  },
+  {
+    date: new Date("04-04-2022"),
+    d: [2052, 2071],
+  },
+  {
+    date: new Date("04-16-2022"),
+    d: [2104, 2170],
+  },
+  {
+    date: new Date("05-13-2022"),
+    d: [2086, 2267],
+  },
+  {
+    date: new Date("06-06-2022"),
+    d: [2070, 2267],
+  },
+  {
+    date: new Date("06-18-2022"),
+    d: [2030, 2364],
+  },
+  {
+    date: new Date("07-17-2022"),
+    d: [2220, 2437],
+  },
+  {
+    date: new Date("08-15-2022"),
+    d: [2482, 2558],
+  },
+  {
+    date: new Date("09-13-2022"),
+    d: [2441, 2632],
+  },
+  {
+    date: new Date("10-10-2022"),
+    d: [2371, 2736],
+  },
+  {
+    date: new Date("11-12-2022"),
+    d: [2741, 3033],
+  },
+  {
+    date: new Date("12-13-2022"),
+    d: [2982, 3230],
+  },
+];
+
+proc = (data) => {
+  data.sort((a, b) => a.date - b.date);
+
+  const result = {
+    data: [],
+    label: [],
+  };
+
+  data.forEach((item) => {
+    result.label.push(item.date);
+    item.d.forEach((value, index) => {
+      if (!result.data[index]) {
+        result.data[index] = [];
+      }
+      result.data[index].push(value);
+    });
+  });
+
+  return result;
+};
+
+const getGrowth = (data) => {
+  data.sort((a, b) => a.date - b.date);
+
+  const growth = [];
+  for (let i = 1; i < data.length; i++) {
+    const period =
+      (data[i].date - data[i - 1].date) / (31 * 1000 * 60 * 60 * 24);
+    growth.push(
+      data[i].d.map((value, index) => (value - data[i - 1].d[index]) / period)
+    );
   }
 
-  return date;
+  const result = {
+    data: [],
+    label: [],
+    grown: [],
+    avg: [],
+  };
+
+  data.forEach((item) => {
+    result.label.push(item.date);
+    item.d.forEach((value, index) => {
+      if (!result.data[index]) {
+        result.data[index] = [];
+      }
+      result.data[index].push(value);
+    });
+  });
+
+  growth.forEach((item) => {
+    item.forEach((value, index) => {
+      if (!result.grown[index]) {
+        result.grown[index] = [];
+      }
+      if (value < 1000) result.grown[index].push(value);
+      else result.grown[index].push(0);
+    });
+  });
+  // cambiar esto para que calcule la media de los incrementos
+  result.grown.forEach((values) => {
+    result.avg.push(values.reduce((a, b) => a + b) / values.length);
+  });
+
+  const response = {
+    data: result.grown,
+    label: result.label,
+  };
+
+  return response;
 };
 
-const sourceData = {
-  data: [
-    [2048, 2049, 2072, 2087, 2069, 2217],
-    [2086, 2185, 2267, 2267, 2364, 2437],
-  ],
-  label: [
-    new Date("04-07-2022"),
-    new Date("05-06-2022"),
-    new Date("05-22-2022"),
-    new Date("06-04-2022"),
-    new Date("06-25-2022"),
-    new Date("07-14-2022"),
-  ],
-};
+var chart = createChart(
+  "chart",
+  createData(proc(sourceData1), colorArray1),
+  options
+);
 
-var chart = createChart("chart", createData(sourceData, colorArray1), options);
+var chart1 = createChart2(
+  "chart1",
+  createData(getGrowth(sourceData1), colorArray1),
+  options
+);
+
+console.log(getGrowth(sourceData1));
 
 let a = 0;
 
 const action = (number) => {
-  if (number === 1) {
-    chart.options.scales.x.min = "2022-01-01 00:00:00";
-    chart.options.scales.x.max = "2022-12-31 00:00:00";
+  console.log({ number }, { mode });
+  if (number === 3) {
+    if (mode === 2) {
+      currentDate.setMonth(currentDate.getMonth() - 6);
+      previousMonth.setMonth(previousMonth.getMonth() - 6);
+    }
+
+    if (mode === 1) {
+      currentDate.setMonth(currentDate.getMonth() - 12);
+      previousMonth.setMonth(previousMonth.getMonth() - 12);
+    }
+
+    chart.options.scales.x.max = currentDate.toISOString();
+    chart.options.scales.x.min = previousMonth.toISOString();
+
+    chart.update("none");
+  }
+  if (number === 4) {
+    if (mode === 2) {
+      currentDate.setMonth(currentDate.getMonth() + 6);
+      previousMonth.setMonth(previousMonth.getMonth() + 6);
+    }
+
+    if (mode === 1) {
+      currentDate.setMonth(currentDate.getMonth() + 12);
+      previousMonth.setMonth(previousMonth.getMonth() + 12);
+    }
+
+    chart.options.scales.x.max = currentDate.toISOString();
+    chart.options.scales.x.min = previousMonth.toISOString();
+
+    chart.update("none");
+  }
+};
+
+const setMode = (m) => {
+  mode = m;
+  if (m === 2) {
+    currentDate = new Date();
+    previousMonth = new Date();
+
+    previousMonth.setMonth(previousMonth.getMonth() - 6);
+
+    chart.options.scales.x.max = currentDate.toISOString();
+    chart.options.scales.x.min = previousMonth.toISOString();
+
     chart.options.scales.x.time.unit = "month";
 
-    // chart.update();
     chart.update("none");
   }
 
-  if (number === 2) {
-    chart.options.scales.x.min = "2022-11-01 00:00:00";
-    chart.options.scales.x.max = "2022-12-31 00:00:00";
+  if (m === 1) {
+    currentDate = new Date();
+    previousMonth = new Date();
 
-    chart.options.scales.x.time.unit = "day";
+    previousMonth.setMonth(previousMonth.getMonth() - 12);
 
-    // chart.update();
+    chart.options.scales.x.max = currentDate.toISOString();
+    chart.options.scales.x.min = previousMonth.toISOString();
+
+    chart.options.scales.x.time.unit = "month";
+
+    chart.update("none");
+  }
+
+  if (m === 0) {
+    previousMonth.setMonth(previousMonth.getMonth() - 12);
+
+    chart.options.scales.x.max = "auto";
+    chart.options.scales.x.min = "auto";
+
+    chart.options.scales.x.time.unit = "month";
+
     chart.update("none");
   }
 };
